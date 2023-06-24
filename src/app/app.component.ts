78310d725cd92a545d311ca7e1cce9285788880b
Miafargo <isuurii@gmail.com>
2023-06-24 18:28:39 +0900

    finished hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HeroComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
