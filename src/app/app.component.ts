2d531c32f5b62f90676365ec4311b92a649847a7
Miafargo <isuurii@gmail.com>
2023-06-25 18:03:47 +0900

    started mid section need work on grid styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
+import { MidComponent } from './mid/mid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,HeroComponent],
+  imports: [CommonModule,HeroComponent,MidComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/mid/mid.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mid',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mid.component.html',
+  styleUrls: ['./mid.component.css']
+})
+export class MidComponent {
+
+}
