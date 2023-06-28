96bb8c2a0b025087bbbc7520e73c4c6c190832d1
Miafargo <isuurii@gmail.com>
2023-06-28 13:08:06 +0900

    finished client section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { MidComponent } from './mid/mid.component';
+import { ClientComponent } from './client/client.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,HeroComponent,MidComponent],
+  imports: [CommonModule, HeroComponent, MidComponent, ClientComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'sunnyside';
