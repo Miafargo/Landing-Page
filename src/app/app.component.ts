d78c56cb33022b4a14221b41feac74c91537908a
Miafargo <isuurii@gmail.com>
2023-06-29 15:29:46 +0900

    finished all sections and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { MidComponent } from './mid/mid.component';
 import { ClientComponent } from './client/client.component';
+import { PicsComponent } from './pics/pics.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeroComponent, MidComponent, ClientComponent],
+  imports: [CommonModule, HeroComponent, MidComponent, ClientComponent,PicsComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
